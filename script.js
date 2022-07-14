197607af384f6062543d4be2b3a80c519a09808c
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-14 19:21:47 +0200

    modifiche css

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const friends = new Vue({
             {
               name: 'Michele',
               avatar: '_1',
+              url:`https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg`
             },
             {
               name: 'Fabio',
@@ -24,6 +25,38 @@ const friends = new Vue({
               avatar: '_4',
             },
             {
+                name: 'Michele',
+                avatar: '_1',
+              },
+              {
+                name: 'Fabio',
+                avatar: '_2',
+              },
+              {
+                name: 'Samuele',
+                avatar: '_3',
+              },
+              {
+                name: 'Luisa',
+                avatar: '_4',
+              },
+              {
+                name: 'Michele',
+                avatar: '_1',
+              },
+              {
+                name: 'Fabio',
+                avatar: '_2',
+              },
+              {
+                name: 'Samuele',
+                avatar: '_3',
+              },
+              {
+                name: 'Luisa',
+                avatar: '_4',
+              },
+              {
                 name: 'Michele',
                 avatar: '_1',
               },
